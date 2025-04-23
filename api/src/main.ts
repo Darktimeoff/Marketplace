643700f33f7418e6d5f98524c67c543a8dbc4bfa
Yevhenii Korolikhin <yevheniikorolihin@gmail.com>
2025-04-23 20:35:13 +0300

    feat(api): add development command for api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { AppModule } from './app/app.module'
 import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
+    const host = process.env.HOST ?? 'localhost'
+    const port = process.env.PORT ?? 3000
+    const dbName = process.env.DB_NAME ?? 'api'
+
     const app = await NestFactory.create(AppModule)
-    await app.listen(process.env.PORT ?? 3000)
+    await app.listen(port)
+    Logger.log('Marketplace API started ', {
+        db: dbName,
+        url: `http://${host}:${port}`,
+        context: 'Bootstrap',
+        pid: process.pid,
+    })
 }
 
 void bootstrap().catch((err: unknown) => {
