7d9ee0617d2e1cd53fc0c41acbb213909decd4b0
Yevhenii Korolikhin <yevheniikorolihin@gmail.com>
2025-05-10 17:52:24 +0300

    feat(api): add brand table and link with product

---
+++ api/prisma/schema/migrations/20250510141544_add_brand_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Brand" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Brand_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Brand_name_key" ON "Brand"("name");
