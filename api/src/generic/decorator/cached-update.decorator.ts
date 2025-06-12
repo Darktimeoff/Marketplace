d7fca6b43b7f9c6ff6e97b5b7368863760a87598
Yevhenii Korolikhin <yevheniikorolihin@gmail.com>
2025-06-12 18:31:18 +0300

    feat(api): add CachedUpdate decorator for caching results of method calls

---
+++ api/src/generic/decorator/cached-update.decorator.ts
@@ -0,0 +1,48 @@
+import { Inject, Logger } from '@nestjs/common'
+import { CacheService } from '@/generic/cache/cache.service'
+import { MethodDecoratorType } from '@/generic/type/method-decorator.type'
+import { CacheKeyFactory } from './сached.decorator'
+import { CacheTTLEnum } from '@/generic/cache/cache-ttl.enum'
+
+export function CachedUpdate<TResult, TArgs extends unknown[] = unknown[]>(
+    key: string | CacheKeyFactory<TArgs>,
+    ttl: CacheTTLEnum = CacheTTLEnum.ONE_DAY
+): MethodDecoratorType<TResult, TArgs> {
+    const injectCacheService = Inject(CacheService)
+
+    return function (
+        target,
+        propertyKey,
+        descriptor: {
+            cacheServiceDecorator?: CacheService
+        } & PropertyDescriptor
+    ) {
+        injectCacheService(target, 'cacheServiceDecorator')
+        const originalMethod = descriptor.value as (...args: TArgs) => Promise<TResult>
+
+        if (!originalMethod) {
+            throw new Error(`Method ${String(propertyKey)} not found`)
+        }
+
+        descriptor.value = async function (...args: TArgs): Promise<TResult> {
+            const cacheService = this.cacheServiceDecorator
+            const logger = new Logger(`${target.constructor.name}::${propertyKey.toString()}`)
+
+            if (!cacheService) {
+                throw new Error('Cache service not found')
+            }
+
+            const cacheKey = typeof key === 'string' ? key : key(...args)
+
+            const result = await originalMethod.apply(this, args)
+
+            const updated = await cacheService.set(cacheKey, result, ttl)
+
+            logger.debug(`${updated ? 'Updated' : 'Not found'} cache: ${cacheKey}`)
+
+            return result
+        }
+
+        return descriptor
+    }
+}
